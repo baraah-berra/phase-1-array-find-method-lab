56c06462ac527855d7847b56919e54d6d4b6f615
baraah-berra <baraahalsaidramadan@gmail.com>
2023-07-18 11:45:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Sample data
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  // Function to find the year of a Superbowl win
+  function superbowlWin(records) {
+    // Use the find() method to search for a win (result: "W")
+    const winRecord = records.find((record) => record.result === "W");
+  
+    // If a win is found, return the year; otherwise, return undefined
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  // Example usage:
+  const winYear = superbowlWin(record);
+  console.log(winYear); // Output: "2015" (if a win is found)
+  
